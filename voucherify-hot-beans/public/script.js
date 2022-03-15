889e88ca372d58d33a14ce4fb83f31f64f4e7922
Michał <fulczyk.michal@gmail.com>
2022-03-15 11:59:54 +0100

    finishing the first part

--- voucherify-hot-beans/public/script.js
+++ voucherify-hot-beans/public/script.js
@@ -26,7 +26,7 @@ window.addEventListener("load", () => {
       price: "41.98",
     },
   ];
-  cartSummary.innerHTML = items
+  cartSummary.innerHTML = `<h2>Item summary (4)</h2> ${items
     .map(
       (item, index) =>
         `<div class='item' key=${index}>
@@ -46,35 +46,13 @@ window.addEventListener("load", () => {
                     <button class="remove-button">Remove</button>
                    </div>`
     )
-    .join("");
-  function addProductPrices(items) {
-    return items
-      .map((item) => {
-        return parseFloat(item.price) * parseInt(item.quantity);
-      })
-      .reduce((partialSum, a) => partialSum + a, 0)
-      .toFixed(2);
-  }
-
-  function incrementQuantity(index) {
-    console.log(items[index]);
-    cartSummary.render();
-    return items[index].quantity++;
-  }
-
-  function decrementQuantity(index) {
-    if (items[index].quantity < 1) return;
-
-    return (e) => {
-      items[index].quantity--;
-    };
-  }
+    .join("")}`;
 
   const buttonsToAddIncrement = document.getElementsByClassName("increment");
   const buttonsToAddDecrement = document.getElementsByClassName("decrement");
   const buttonToCheckVoucherCode =
     document.getElementsByClassName("checkVoucherCode");
-
+  console.log(buttonsToAddIncrement);
   for (let i = 0; i < buttonsToAddIncrement.length; i++) {
     buttonsToAddIncrement[i].addEventListener("click", () =>
       incrementQuantity(i)
@@ -88,4 +66,101 @@ window.addEventListener("load", () => {
   buttonToCheckVoucherCode[0].addEventListener("click", () =>
     checkVoucher(document.getElementById("voucherCode").value)
   );
+
+  function addProductPrices(items) {
+    return items
+      .map((item) => {
+        return parseFloat(item.price) * parseInt(item.quantity);
+      })
+      .reduce((partialSum, a) => partialSum + a, 0)
+      .toFixed(2);
+  }
+
+  function incrementQuantity(inde2) {
+    items[inde2].quantity = items[inde2].quantity + 1;
+    cartSummary.innerHTML = `<h2>Item summary (4)</h2> ${items
+      .map(
+        (item, index) =>
+          `<div class='item' key=${index}>
+                      <img src='/'/>
+                      <div class='name-and-description'>
+                        <span>${item.productName}</span>
+                        <span>${item.productDescription}</span>
+                      </div>
+                      <div class="form-and-button-holder">
+                        <button class='decrement' id="decrementQuantity-${index}">-</button>
+                        <form>
+                        <input type="number" value="${item.quantity}"/>
+                        </form>
+                        <button class='increment' id="incrementQuantity-${index}">+</button>
+                      </div>
+                      <span class="price">$${item.price}</span>
+                      <button class="remove-button">Remove</button>
+                     </div>`
+      )
+      .join("")}`;
+    const buttonsToAddIncrement = document.getElementsByClassName("increment");
+    const buttonsToAddDecrement = document.getElementsByClassName("decrement");
+    const buttonToCheckVoucherCode =
+      document.getElementsByClassName("checkVoucherCode");
+    console.log(buttonsToAddIncrement);
+    for (let i = 0; i < buttonsToAddIncrement.length; i++) {
+      buttonsToAddIncrement[i].addEventListener("click", () =>
+        incrementQuantity(i)
+      );
+    }
+    for (let i = 0; i < buttonsToAddDecrement.length; i++) {
+      buttonsToAddDecrement[i].addEventListener("click", () =>
+        decrementQuantity(i)
+      );
+    }
+    buttonToCheckVoucherCode[0].addEventListener("click", () =>
+      checkVoucher(document.getElementById("voucherCode").value)
+    );
+  }
+
+  function decrementQuantity(inde2) {
+    if (items[inde2].quantity < 1) return;
+
+    items[inde2].quantity = items[inde2].quantity - 1;
+    cartSummary.innerHTML = `<h2>Item summary (4)</h2> ${items
+      .map(
+        (item, index) =>
+          `<div class='item' key=${index}>
+                      <img src='/'/>
+                      <div class='name-and-description'>
+                        <span>${item.productName}</span>
+                        <span>${item.productDescription}</span>
+                      </div>
+                      <div class="form-and-button-holder">
+                        <button class='decrement' id="decrementQuantity-${index}">-</button>
+                        <form>
+                        <input type="number" value="${item.quantity}"/>
+                        </form>
+                        <button class='increment' id="incrementQuantity-${index}">+</button>
+                      </div>
+                      <span class="price">$${item.price}</span>
+                      <button class="remove-button">Remove</button>
+                     </div>`
+      )
+      .join("")}`;
+    const buttonsToAddIncrement = document.getElementsByClassName("increment");
+    const buttonsToAddDecrement = document.getElementsByClassName("decrement");
+    const buttonToCheckVoucherCode =
+      document.getElementsByClassName("checkVoucherCode");
+    console.log(buttonsToAddIncrement);
+    for (let i = 0; i < buttonsToAddIncrement.length; i++) {
+      buttonsToAddIncrement[i].addEventListener("click", () =>
+        incrementQuantity(i)
+      );
+    }
+    for (let i = 0; i < buttonsToAddDecrement.length; i++) {
+      buttonsToAddDecrement[i].addEventListener("click", () =>
+        decrementQuantity(i)
+      );
+    }
+    buttonToCheckVoucherCode[0].addEventListener("click", () =>
+      checkVoucher(document.getElementById("voucherCode").value)
+    );
+  }
 });
